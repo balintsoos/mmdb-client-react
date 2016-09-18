54228a6a54bbc111be2013273d82f5356c77bdc1
Balint Soos <soosbalint95@gmail.com>
2016-09-18 23:54:27 +0200

    category: render list items

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,26 +1,29 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_year_data"] }] */
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 import Helmet from 'react-helmet'
 
 class Category extends Component {
+  renderListItem(item) {
+    const url = `${this.props.location.pathname}/view/${item.id}`
+
+    return (
+      <Link to={url} key={item.id}>
+        <div className="list-item">
+          <span>{`${item.data.title} (${item.data._year_data})`}</span>
+        </div>
+      </Link>
+    )
+  }
+
   render() {
     return (
       <div>
         <Helmet title={`${this.props.params.category}`} />
 
-        <ul>
-          {
-            this.props.category.map((item) => {
-              const url = `${this.props.location.pathname}/view/${item.id}`
-
-              return (
-                <li key={item.id}>
-                  <Link to={url}>{item.data.title}</Link>
-                </li>
-              )
-            }, this)
-          }
-        </ul>
+        <div className="category-item-list-wrapper">
+          {this.props.category.map(item => this.renderListItem(item), this)}
+        </div>
       </div>
     )
   }
